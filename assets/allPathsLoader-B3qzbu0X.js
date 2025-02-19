41cb3af853706a5046dadaced588be959cafef6c
Таня Самойленко <ania000535@gmail.com>
2025-02-19 20:31:38 +0100

    Updates

---
+++ assets/allPathsLoader-B3qzbu0X.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-CiGrBi0s.js","assets/index-QcAPZC1N.js","assets/index-DDC8wvLK.js","assets/index-BllK35No.js","assets/index-BBqdb2Px.css"])))=>i.map(i=>d[i]);
+import{_ as o,a as n,b as i}from"./index-BllK35No.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-CiGrBi0s.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
