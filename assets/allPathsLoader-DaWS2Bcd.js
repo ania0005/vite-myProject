c939aa57afec6831271b09b9dbbc6c30316cd83f
Таня Самойленко <ania000535@gmail.com>
2025-02-25 21:52:54 +0100

    Updates

---
+++ assets/allPathsLoader-DaWS2Bcd.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-5-h8GsWl.js","assets/index-DxLGmC6o.js","assets/index-CsS_OkTl.js","assets/index-CwuOvYpG.js","assets/index-BR09RhDx.css"])))=>i.map(i=>d[i]);
+import{_ as o,a as n,b as i}from"./index-CwuOvYpG.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-5-h8GsWl.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
