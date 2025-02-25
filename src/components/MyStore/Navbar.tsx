4c739ce0ff8251c42d0cf9661aae19729a19b095
Таня Самойленко <ania000535@gmail.com>
2025-02-25 22:02:03 +0100

    add

--- src/components/MyStore/Navbar.tsx
+++ src/components/MyStore/Navbar.tsx
@@ -1,28 +1,25 @@
 import React from "react";
 import { AiOutlineUser } from "react-icons/ai";
 import { FiShoppingCart } from "react-icons/fi";
-import "../../../tailwind.css"
-
-
+import "../../../tailwind.css";
 
 const Navbar = () => {
   return (
     <div className="pt-4 bg-white top-0 sticky">
       <div className="container">
         <div className="flex justify-between items-center">
-            <h1 className="text-4xl font-bold cursor-pointer">Shopping</h1>
-        <div className="flex gap-4 md:gap-8 items-center">
-          <div className="md:flex items-center gap-3 hidden">
-            <div className="rounded-full border-2 border-gray-300 
+          <h1 className="text-4xl font-bold cursor-pointer">Shopping</h1>
+          <div className="flex gap-4 md:gap-8 items-center">
+            <div className="md:flex items-center gap-3 hidden">
+              <div
+                className="rounded-full border-2 border-gray-300 
             text-gray-500 text-3xl w-12 h-12 flex items-center justify-center 
-            cursor-pointer hover:bg-gray-200 transition-all duration-200 ">
-              <AiOutlineUser /> 
+            cursor-pointer hover:bg-gray-200 transition-all duration-200 "
+              >
+                <AiOutlineUser />
+              </div>
             </div>
-            <h1 className="text-3xl font-bold underline text-center text-blue-500">
-      Hello, Tailwind CSS!
-    </h1>
           </div>
-        </div>
         </div>
       </div>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-project/", // Укажите имя репозитория
+  base: "/vite-myProject/", // Укажите имя репозитория
   build: {
     outDir: "dist",
   },
